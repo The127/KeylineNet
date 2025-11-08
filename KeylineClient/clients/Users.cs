928d856af5d7880abbbff5fbf4ee8e93002c52de
karo <karolin.kostial@gmail.com>
2025-11-08 20:38:20 +0100

    Add user retrieval and update support
    Extended `IUsersClient` to include user retrieval and update (`IUserClient`) functionality. Implemented DTOs and methods for fetching and patching individual users. Added new fields to `GetUserResponseDto` for extended user details.

--- KeylineClient/clients/Users.cs
+++ KeylineClient/clients/Users.cs
@@ -7,25 +7,21 @@ namespace KeylineClient.clients;
 [UsedImplicitly]
 public class ListUsersResponseDto
 {
-    [JsonPropertyName("id")]
-    public Guid Id { get; set; }
+    [JsonPropertyName("id")] public Guid Id { get; set; }
 
-    [JsonPropertyName("username")]
-    public required string Username { get; set; }
+    [JsonPropertyName("username")] public required string Username { get; set; }
 
-    [JsonPropertyName("displayName")]
-    public required string DisplayName { get; set; }
-    
-    [JsonPropertyName("primaryEmail")]
-    public required string PrimaryEmail { get; set; }
-    
-    [JsonPropertyName("isServiceUser")]
-    public bool IsServiceUser { get; set; }
+    [JsonPropertyName("displayName")] public required string DisplayName { get; set; }
+
+    [JsonPropertyName("primaryEmail")] public required string PrimaryEmail { get; set; }
+
+    [JsonPropertyName("isServiceUser")] public bool IsServiceUser { get; set; }
 }
 
 public interface IUsersClient
 {
     Task<PagedResponse<ListUsersResponseDto>> ListAsync(CancellationToken cancellationToken = default);
+    IUserClient this[Guid userId] { get; }
 }
 
 public class UsersClient(
@@ -40,8 +36,77 @@ public class UsersClient(
 
         var response = await httpClient.GetAsync(uri, cancellationToken);
         response.EnsureSuccessStatusCode();
-        
+
         var result = await response.Content.ReadFromJsonAsync<PagedResponse<ListUsersResponseDto>>(cancellationToken);
         return result!;
+    }
+
+    public IUserClient this[Guid userId] =>
+        new UserClient(httpClient, virtualServer, userId);
+}
+
+public class GetUserResponseDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+    
+    [JsonPropertyName("username")]
+    public required string Username { get; set; }
+    
+    [JsonPropertyName("displayName")]
+    public required string DisplayName { get; set; }
+    
+    [JsonPropertyName("primaryEmail")]
+    public required string PrimaryEmail { get; set; }
+    
+    [JsonPropertyName("emailVerified")]
+    public bool EmailVerified { get; set; }
+    
+    [JsonPropertyName("isServiceUser")]
+    public bool IsServiceUser { get; set; }
+
+    [JsonPropertyName("createdAt")] 
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("updatedAt")] 
+    public DateTime UpdatedAt { get; set; }
+}
+
+public class PatchUserRequestDto
+{
+    [JsonPropertyName("displayName")]
+    public string? DisplayName { get; set; }
+}
+
+public interface IUserClient
+{
+    Task<GetUserResponseDto> GetAsync(CancellationToken cancellationToken = default);
+    Task PatchAsync(PatchUserRequestDto request, CancellationToken cancellationToken = default);
+}
+
+public class UserClient(
+    HttpClient httpClient,
+    string virtualServer,
+    Guid userId
+)
+    : IUserClient
+{
+    public async Task<GetUserResponseDto> GetAsync(CancellationToken cancellationToken = default)
+    {
+        var uri = new Uri($"api/virtual-servers/{virtualServer}/users/{userId}", UriKind.Relative);
+
+        var response = await httpClient.GetAsync(uri, cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<GetUserResponseDto>(cancellationToken);
+        return result!;
+    }
+
+    public async Task PatchAsync(PatchUserRequestDto request, CancellationToken cancellationToken = default)
+    {
+        var uri = new Uri($"api/virtual-servers/{virtualServer}/users/{userId}", UriKind.Relative);
+
+        var response = await httpClient.PatchAsJsonAsync(uri, request, cancellationToken);
+        response.EnsureSuccessStatusCode();
     }
 }
