861462396c9a3c0d323e506380499b5711fb5a8f
karo <karolin.kostial@gmail.com>
2025-11-09 14:25:50 +0100

    Add support for application creation endpoint
    Extended `IApplicationsClient` with `CreateAsync` method for creating applications. Added `CreateApplicationRequestDto` and `CreateApplicationResponseDto` for request and response handling. Implemented necessary HTTP POST logic.

--- KeylineClient/clients/Applications.cs
+++ KeylineClient/clients/Applications.cs
@@ -23,9 +23,42 @@ public class ListApplicationsResponseDto
     public bool SystemApplication { get; set; }
 }
 
+[UsedImplicitly]
+public class CreateApplicationRequestDto
+{
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+    
+    [JsonPropertyName("displayName")]
+    public required string DisplayName { get; set; }
+    
+    [JsonPropertyName("redirectUris")]
+    public required List<string> RedirectUris { get; set; }
+    
+    [JsonPropertyName("postLogoutUris")]
+    public List<string> PostLogoutUris { get; set; } = [];
+    
+    [JsonPropertyName("type")]
+    public required string Type { get; set; }
+    
+    [JsonPropertyName("accessTokenHeaderType")]
+    public string? AccessTokenHeaderType { get; set; }
+}
+
+[UsedImplicitly]
+public class CreateApplicationResponseDto
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+    
+    [JsonPropertyName("secret")]
+    public string? Secret { get; set; }
+}
+
 public interface IApplicationsClient
 {
     Task<PagedResponse<ListApplicationsResponseDto>> List(CancellationToken cancellationToken = default);
+    Task<CreateApplicationResponseDto> CreateAsync(CreateApplicationRequestDto request, CancellationToken cancellationToken = default);
 }
 
 public class ApplicationsClient(
@@ -42,6 +75,17 @@ public class ApplicationsClient(
         response.EnsureSuccessStatusCode();
 
         var result = await response.Content.ReadFromJsonAsync<PagedResponse<ListApplicationsResponseDto>>(cancellationToken);
+        return result!;
+    }
+
+    public async Task<CreateApplicationResponseDto> CreateAsync(CreateApplicationRequestDto request, CancellationToken cancellationToken = default)
+    {
+        var uri = new Uri($"api/virtual-servers/{virtualServer}/projects/{projectSlug}/applications", UriKind.Relative);
+
+        var response = await httpClient.PostAsJsonAsync(uri, request, cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<CreateApplicationResponseDto>(cancellationToken);
         return result!;
     }
 }
